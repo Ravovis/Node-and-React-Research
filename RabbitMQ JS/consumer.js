1df95b7e4da118d2583ba1e5a1997a87604bae8c
Serhii Postolenko <ravovis@gmail.com>
2021-09-16 15:11:57 +0300

    How to clear queue in rabbitMQ

---
+++ RabbitMQ JS/consumer.js
@@ -0,0 +1,28 @@
+
+var amqp = require("amqplib");
+
+connect();
+ 
+async function connect(){
+    try{
+        const connection = await amqp.connect("amqp://localhost:5672");
+        const channel = await connection.createChannel();
+        const result = await channel.assertQueue("jobs");
+
+        channel.consume("jobs", message=>{
+            const input =JSON.parse(message.content.toString());
+            console.log(`Recieved.job with input ${input.number}`)
+            
+            console.log(input.number)
+            if(input.number == 7)// not === because it's a string
+            {
+                channel.ack(message);
+            } 
+        }) 
+
+        console.log("Waiting for messages...")
+        }
+    catch (ex){
+        console.log(ex);
+    }
+}
---
+++ RabbitMQ JS/publisher.js
@@ -0,0 +1,19 @@
+
+var amqp = require("amqplib");
+
+const mesg = {number:process.argv[2]}
+
+connect();  
+
+async function connect(){ 
+    try{
+        const connection = await amqp.connect("amqp://localhost:5672");
+        const channel = await connection.createChannel();
+        const result = await channel.assertQueue("jobs");
+        channel.sendToQueue("jobs",Buffer.from(JSON.stringify(mesg)));
+        console.log(`Job sent succesfully ${mesg.number}`);
+    } 
+    catch (ex){
+        console.log(ex);
+    }
+}
