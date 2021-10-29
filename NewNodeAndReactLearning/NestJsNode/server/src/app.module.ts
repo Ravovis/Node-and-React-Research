c211488b5a87a9ea11d6a4a78dfd91fbf44fb953
Ravovis <ravovis@gmail.com>
2021-10-29 06:24:16 -0800

    Added mini background job with cron

--- NewNodeAndReactLearning/NestJsNode/server/src/app.module.ts
+++ NewNodeAndReactLearning/NestJsNode/server/src/app.module.ts
@@ -1,14 +1,20 @@
 import { Module } from '@nestjs/common';
+import { ScheduleModule } from '@nestjs/schedule';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MyDocumentSchema } from './schemas/myDocument.schema';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
+import { TasksService } from './tasksService';
 
 @Module({
   controllers: [AppController],
-  providers: [AppService],
-  imports: [MongooseModule.forFeature([{ name: 'MyDocument', schema: MyDocumentSchema }]), MongooseModule.forRoot('mongodb://localhost:27017/MongoDbResearching'), UserModule, AuthModule]
+  providers: [AppService,TasksService],
+  imports: [MongooseModule.forFeature([{ name: 'MyDocument', schema: MyDocumentSchema }]),
+  MongooseModule.forRoot('mongodb://localhost:27017/MongoDbResearching'),
+    UserModule,
+    AuthModule,
+  ScheduleModule.forRoot()]
 })
-export class AppModule {}
+export class AppModule { }
